27d8bd62230826f924f2babd5015020da5239b2d
Kruemmelspalter <kruemmelspalter@duck.com>
2022-12-03 23:01:06 +0100

    irgendein random shit

--- src/src.ino
+++ src/src.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+
+#include "rgb_lcd.h"
 
 /// SENSORS
 
@@ -10,9 +13,12 @@
 // resistance in Ohm
 #define MAX_CURRENT_RESISTOR 10
 #define MAX_VOLT_DIVIDER_RATIO 2
+#define LOAD_CURRENT_RESISTOR 10
 
+#define DEBUG
 
-// #define DEBUG
+/// PERIPHERALS
+rgb_lcd lcd;
 
 void sensorSetup() {
   pinMode(MAX_CURRENT_RELAY, OUTPUT);
@@ -26,8 +32,21 @@ void sensorSetup() {
   digitalWrite(LOAD_RELAY, HIGH); // TODO do not
 }
 
-// returns power in milliwatt
-unsigned long measureMaxPower(int halfMeasurementDuration = 1) {
+// in mA
+unsigned long measureLoadCurrent() {
+	int rawVoltage = analogRead(LOAD_CURRENT_SENSOR);
+
+	// in mV
+	unsigned long voltage = (unsigned long) (rawVoltage+1) * 5000 / 1024;
+
+	// in mA
+	unsigned long current = (unsigned long) voltage / LOAD_CURRENT_RESISTOR;
+	
+	return current;
+}
+
+// returns power in mW
+unsigned long measureMaxPower(int halfMeasurementDuration = 10) {
   digitalWrite(LOAD_RELAY, LOW);
   digitalWrite(MAX_CURRENT_RELAY, HIGH);
   delay(halfMeasurementDuration);
@@ -42,30 +61,47 @@ unsigned long measureMaxPower(int halfMeasurementDuration = 1) {
   // in mW
   unsigned long wattage = (unsigned long) (voltage * voltage) / MAX_CURRENT_RESISTOR / 1000;
 
-  #ifdef DEBUG
-    Serial.print("Raw: ");
-    Serial.print(rawVoltage);
-    Serial.print("; mV: ");
-    Serial.print(voltage);
-    Serial.print("; mW: ");
-    Serial.print(wattage);
-    Serial.println();
-  #endif
-
   return wattage;
+}
+
+// in mV
+unsigned long measureVoltage() {
+  int rawVoltage = analogRead(MAX_CURRENT_SENSOR); // in 0-1023
+
+  // 0-1023 to mV
+  return (unsigned long) (rawVoltage+1) * 5000 * MAX_VOLT_DIVIDER_RATIO / 1024;
 }
 
 
 /// MAIN
 
+char buff[10];
+
 void setup() {
-  Serial.begin(115200);
+  lcd.begin(16, 2);
+
+  lcd.setRGB(0xFF, 0xFF, 0xFF);
 
   sensorSetup();
 }
 
 void loop() {
-  Serial.println(measureMaxPower(10));
+  unsigned long sumVolt = 0;
+  unsigned long sumCur = 0;
+  for(int i = 0; i<5; i++) {
+    sumVolt += measureVoltage();
+    sumCur += measureLoadCurrent();
+    delay(200);
+  }
   
-  delay(500);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  sprintf(buff, "%04d", sumVolt/5);
+  lcd.print("U="+(String) buff+"mV");
+  lcd.setCursor(8,0);
+  sprintf(buff, "%04d", sumCur/5);
+  lcd.print("I="+(String)buff+"mA");
+  lcd.setCursor(0,1);
+  sprintf(buff, "%04d", measureMaxPower());
+  lcd.print("P="+ (String) buff+"mW");
 }
